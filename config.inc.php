0f06ea7a9445a9489b7021c0773b4877f8b14ae4
avg <1994.avg@gmail.com>
2019-08-02 03:20:56 +0530

    conf

--- config.inc.php
+++ config.inc.php
@@ -28,7 +28,8 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = $_ENV['MYSQL_HOST'];
+$cfg['Servers'][$i]['host'] = 'remotemysql.com';
+$cfg['Servers'][$i]['port']          = '3306';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
